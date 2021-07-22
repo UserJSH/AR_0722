180324a1bf03ebe072cc7e2451ae5918055715dd
UserJSH <jsh495062@gmail.com>
2021-07-22 18:11:43 +0900

    A2_0722_PM

--- A2_AR_0720/Assets/Scenes/BoxMoving/BoxController.cs
+++ A2_AR_0720/Assets/Scenes/BoxMoving/BoxController.cs
@@ -1,16 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoxController : MonoBehaviour
 {
-    private const float cameraDistance = 7.5f; //
-    private float positionY = 0.4f; // Y축 포지션
-    [SerializeField] private GameObject[] obj; // 생성할 오브젝트
+    protected const float cameraDistance = 7.5f; //
+    protected float positionY = 0.4f; // Y축 포지션
+    [SerializeField] protected GameObject[] obj; // 생성할 오브젝트
 
-    private Camera mainCam; // 메인카메라
-    private GameObject HoldingObj; // 손으로 잡은 오브젝트
-    private Vector3 InputPos; // input posision
+    protected Camera mainCam; // 메인카메라
+    protected GameObject HoldingObj; // 손으로 잡은 오브젝트
+    protected Vector3 InputPos; // input posision
     // Start is called before the first frame update
     void Start()
     {
@@ -22,8 +23,12 @@ public class BoxController : MonoBehaviour
     {
         //어느위치에 오브젝트를 생성시킬지 position을 받아옴
         var pos = mainCam.ViewportToWorldPoint(new Vector3(0.5f, positionY, mainCam.nearClipPlane * cameraDistance));
+
+        //랜덤
+        var index = UnityEngine.Random.Range(0, obj.Length);
+
         //오브젝트 생성
-        var cube = Instantiate(obj[0], pos, Quaternion.identity, mainCam.transform);
+        var cube = Instantiate(obj[index], pos, Quaternion.identity, mainCam.transform);
         //RigidBody
         var rigid = cube.GetComponent<Rigidbody>();
         rigid.useGravity = false;
@@ -44,5 +49,57 @@ public class BoxController : MonoBehaviour
         {
             init();
         }
+
+        //오브젝트 이동
+        if (HoldingObj)
+        {
+            //오브젝트 놓기
+            if (TouchHelper.IsUp)
+            {
+                OnPut(InputPos);
+                HoldingObj = null;
+                return;
+            }
+
+            Move(InputPos);
+        }
+
+        //오브젝트 잡기
+        if (!TouchHelper.IsDown) return;
+
+        RaycastHit hit;
+        if (Physics.Raycast(mainCam.ScreenPointToRay(InputPos), out hit, mainCam.farClipPlane))
+        {
+            if (hit.transform.tag.Equals("Player"))
+            {
+                HoldingObj = hit.transform.gameObject;
+                OnHold();
+            }
+        }
+    }
+
+    protected virtual void OnPut(Vector3 pos)
+    {
+        HoldingObj.GetComponent<Rigidbody>().useGravity = true;
+        HoldingObj.transform.SetParent(null); // 자식해제
+    }
+
+    protected virtual void OnHold()
+    {
+        HoldingObj.GetComponent<Rigidbody>().useGravity = false;
+
+        //잡은 오브젝트를 카메라의 자식으로
+        HoldingObj.transform.SetParent(mainCam.transform); // 자식등록
+        HoldingObj.transform.rotation = Quaternion.identity;
+        HoldingObj.transform.position = mainCam.ViewportToWorldPoint(new Vector3(0.5f, positionY, mainCam.nearClipPlane * cameraDistance));
+    }
+
+    void Move(Vector3 pos)
+    {
+        //오브젝트 이동
+        //z축을 고정
+        pos.z = mainCam.nearClipPlane * cameraDistance;
+        HoldingObj.transform.position = Vector3.Lerp(HoldingObj.transform.position, mainCam.ScreenToWorldPoint(pos), Time.deltaTime * 7f);
     }
 }
+
